e4cdc3f191c18cca26606b9a37597e226fdd134c
Sebastián Zapateiro <desarrollo3oai@cartagena.gov.co>
2022-05-27 23:03:50 -0500

    Se cambia el título del ejercicio

--- Modulo-2/01-Conceptos-Basicos/Tarea-03/script.js
+++ Modulo-2/01-Conceptos-Basicos/Tarea-03/script.js
@@ -1,6 +1,6 @@
 
 /*Actividad:
-Hacer un algoritmo que calcule el sueldo de un empleado dados como datos de entrada: el nombre, horas trabajadas y el valor de la hora.
+Un estacionamiento requiere determinar el cobro que debe aplicar a las personas que lo utilizan. Considere que el cobro es con base en las horas que lo disponen y que las fracciones de hora se toman como completas. Realizar el algoritmo que permita determinar el cobro.
 
 Realizado por: Sebastián Zapateiro
 */
